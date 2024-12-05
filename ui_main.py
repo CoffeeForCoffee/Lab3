7e63733a38ca5e94a55cfc33cd469e3faec137cf
Grigory P <grisha9871234@gmail.com>
2024-12-05 15:24:39 +0300

    Launching

---
+++ ui_main.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ui_main.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QSpinBox, QVBoxLayout,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(973, 665)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setStyleSheet(u"background-color: rgb(65, 65, 65)")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setMouseTracking(False)
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setEnabled(True)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy1)
+        self.label.setMaximumSize(QSize(16777215, 16777215))
+        font = QFont()
+        font.setPointSize(30)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(False)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.label.setStyleSheet(u"font-size: 30pt;\n"
+"color: rgb(221, 221, 221);\n"
+"background-color: rgba(255, 255, 255, 50);\n"
+"border: 1px solid rgba(255, 255, 255, 80);\n"
+"border-radius: 7px;\n"
+"")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setEnabled(True)
+        sizePolicy1.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy1)
+        font1 = QFont()
+        font1.setPointSize(20)
+        font1.setBold(True)
+        font1.setItalic(False)
+        font1.setUnderline(False)
+        self.label_2.setFont(font1)
+        self.label_2.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.label_2.setStyleSheet(u"font-size: 20pt;\n"
+"color: rgb(221, 221, 221);\n"
+"background-color: rgba(255, 255, 255, 50);\n"
+"border: 1px solid rgba(255, 255, 255, 80);\n"
+"border-radius: 7px;\n"
+"")
+        self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.spinBox = QSpinBox(self.centralwidget)
+        self.spinBox.setObjectName(u"spinBox")
+        sizePolicy1.setHeightForWidth(self.spinBox.sizePolicy().hasHeightForWidth())
+        self.spinBox.setSizePolicy(sizePolicy1)
+        self.spinBox.setMaximumSize(QSize(16777215, 100))
+        self.spinBox.setStyleSheet(u"")
+        self.spinBox.setWrapping(False)
+        self.spinBox.setFrame(True)
+        self.spinBox.setMaximum(1000000000)
+
+        self.verticalLayout.addWidget(self.spinBox)
+
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        sizePolicy1.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy1)
+        self.pushButton.setMaximumSize(QSize(16777215, 50))
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u043f\u043e\u043c\u043d\u0438\u0442\u0435 \u0447\u0438\u0441\u043b\u043e \u0438 \u0432\u0432\u0435\u0434\u0438\u0442\u0435 \u0435\u0433\u043e.", None))
+        self.label_2.setText("")
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0434\u0442\u0432\u0435\u0440\u0434\u0438\u0442\u044c", None))
+    # retranslateUi
+
