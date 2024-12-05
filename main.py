184a6b88daee433d9592bd3dbbebb4e054b20849
Grigory P <grisha9871234@gmail.com>
2024-12-05 15:24:25 +0300

    Launching

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+from PySide6.QtWidgets import QApplication, QMainWindow
+from ui_main import Ui_MainWindow
+import sys
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
