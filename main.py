fd5c43bc838422809638d5644b02ff099ff5d2fd
Grigory P <grisha9871234@gmail.com>
2024-12-05 17:51:49 +0300

    Random numbers and timer

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QTimer
 from ui_main import Ui_MainWindow
 import sys
+import random
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        self.show_number_temporarily()
+
+    def show_number_temporarily(self):
+        # Генерируем случайное число
+        random_number = random.randint(1, 1000000000)
+        self.label_2.setText(str(random_number))
+
+        # Создаем таймер
+        timer = QTimer(self)
+        timer.setSingleShot(True)
+        timer.timeout.connect(self.clear_label)
+        timer.start(2000)
+
+    def clear_label(self):
+        self.label_2.setText("")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
