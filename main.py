c6fa72c3b24c5b0471eaed2ec89e6dcc7fb0c06b
Grigory P <grisha9871234@gmail.com>
2024-12-10 17:44:01 +0300

    New UI, working code

--- main.py
+++ main.py
@@ -8,22 +8,44 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        self.random_number = None
+        self.pushButton.clicked.connect(self.check_number)
         self.show_number_temporarily()
 
     def show_number_temporarily(self):
         # Генерируем случайное число
-        random_number = random.randint(1, 1000000000)
-        self.label_2.setText(str(random_number))
+        self.random_number = random.randint(1, 1000000000)
+        self.label_2.setText(str(self.random_number))
 
-        # Создаем таймер
+        # Таймер на 2 секунды
         timer = QTimer(self)
         timer.setSingleShot(True)
         timer.timeout.connect(self.clear_label)
         timer.start(2000)
 
-    # Исчезновение числа
     def clear_label(self):
+        # Очистка текста
         self.label_2.setText("")
+
+    def check_number(self):
+        user_number = self.spinBox.value()
+
+        # Совпадение с random_number
+        if user_number == self.random_number:
+            self.label_2.setStyleSheet("background-color: green; color: white;")
+        else:
+            self.label_2.setStyleSheet("background-color: red; color: white;")
+
+        # Таймер для сброса цвета и генерации нового числа
+        reset_timer = QTimer(self)
+        reset_timer.setSingleShot(True)
+        reset_timer.timeout.connect(self.reset_label_and_generate_new)
+        reset_timer.start(2000)
+
+    def reset_label_and_generate_new(self):
+        self.label_2.setStyleSheet("")
+        self.show_number_temporarily()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
